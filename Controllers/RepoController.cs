427f1ebfa5935f4747666625d2e9bb8aa32efa64
Adrián Lazarte <alazarte@lojack.com.ar>
2023-02-18 19:17:46 -0300

    Add a new entry point to add a new file in the repository.

--- Controllers/RepoController.cs
+++ Controllers/RepoController.cs
@@ -2,6 +2,7 @@
 using Octokit;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -119,6 +120,77 @@ namespace TotsChallenge.Controllers
             catch (Exception e)
             {
                 return BadRequest("[TotsChallenge.Controllers.Repo.ListRepo.Error]: " + e.Message);
+            }
+        }
+
+        [HttpPost("/AddFile")]
+        public async Task<IActionResult> AddNewFileToRepo(string repo, string nameFile)
+        {
+            try
+            {
+                this.GetAuth();
+
+                string newNameFile = RemoveSpecialCharacters(nameFile);
+
+                if (newNameFile.Length > 30)
+                {
+                    return BadRequest("The name of the new file add is large, try to use another. The limit is 30 characters.");
+                }
+
+                //Is necesary make control check about repo name because the user can write anything
+                var repositoryList = await client.Repository.GetAllForCurrent();
+
+                foreach (var repoItem in repositoryList)
+                {
+                    if (repoItem.Name.Equals(repo))
+                    {
+                        //Now, is necesary check also if the new name file don't exist inside the repo
+                        int fileExist = await this.SearchFile(repo, nameFile);
+
+                        if (fileExist.Equals(0))
+                        {
+                            //Finally, add the new file
+                            var sb = new StringBuilder("---");
+                            sb.AppendLine();
+                            sb.AppendLine($"date: \"" + DateTime.Now + "\"");
+                            sb.AppendLine($"title: \"New file was added\"");
+                            sb.AppendLine("---");
+                            sb.AppendLine();
+
+                            var (owner, repoName, filePath, branch) = (Environment.GetEnvironmentVariable("USER_GITHUB"), repo,
+                                    "./" + newNameFile + "", "main");
+
+                            await client.Repository.Content.CreateFile(
+                                 owner, repoName, filePath,
+                                 new CreateFileRequest($"First commit for {filePath}", sb.ToString(), branch));
+
+                            return Ok("The file was added to repository successfully");
+                        }
+
+                        return BadRequest("Cannot add a new file with that name file because there another file was used.");
+
+                    }
+                }
+
+                return BadRequest("The repository don't exist.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("[TotsChallenge.Controllers.Repo.AddNewFileToRepo.Error]: " + e.Message);
+            }
+        }
+
+        private async Task<int> SearchFile(string repo, string name)
+        {
+            try
+            {
+                var result = await client.Repository.Content.GetAllContents(Environment.GetEnvironmentVariable("USER_GITHUB"), repo, "./");
+                return result.Where(files => files.Name.Equals(name)).Count();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[TotsChallenge.Controllers.Repo.SearchFile.Error]: " + e.Message);
+                return -1;
             }
         }
 
