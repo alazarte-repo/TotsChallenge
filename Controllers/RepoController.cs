c522d79029c1270c412d9201ae2f2df2a2b25954
Adrián Lazarte <alazarte@lojack.com.ar>
2023-02-18 17:17:51 -0300

    Add repository controller file. The API get authentication code to send request. The code has clean from lineas because it didn't use.

---
+++ Controllers/RepoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TotsChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepoController : ControllerBase
+    {
+        private GitHubClient client;
+
+        [HttpGet]
+        private void GetAuth()
+        {
+            try
+            {
+                //Set the personal access token from GitHub page to get user's credential
+                Credentials credentials = new Credentials(Environment.GetEnvironmentVariable("PERSONAL_ACCESS_TOKEN"));
+                client = new GitHubClient(new ProductHeaderValue(Environment.GetEnvironmentVariable("USER_GITHUB"))) { Credentials = credentials };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[TotsChallenge.Controllers.Repo.GetAuth.Error]: " + e.Message);
+            }
+        }
+    }
+}
